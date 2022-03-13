7005618073a438bedad4e4b771422b7871887821
Kasai Koji <80010105+StreamWest-1629@users.noreply.github.com>
2022-03-14 07:42:04 +0900

    #2: Jupyterから git pushができるようになる

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+printenv GIT_CREDENTIALS > /root/.git-credentials
+git config --global credential.helper "store --file /root/.git-credentials"
+git config --global user.name "${GIT_USERNAME}"
+git config --global user.email ${GIT_USERMAIL}
+
 cd /src
 goofys -f ${DEEPLAB_BUCKETNAME}:.jupyter/ /root/.jupyter/ &
 goofys -f ${DEEPLAB_BUCKETNAME}:dataset/ /dataset/ &
