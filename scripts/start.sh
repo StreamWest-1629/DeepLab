270d419a5c7f41dcb3e8fd054d1e5350925b5547
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2022-03-14 01:14:43 +0900

    #2: シェルスクリプト修正

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,6 @@
 
 goofys -f ${DEEPLAB_BUCKETNAME}:.jupyter/ /root/.jupyter/ &
 goofys -f ${DEEPLAB_BUCKETNAME}:dataset/ /dataset/ &
-python3 -m jupyter lab --port 5515 --ip=0.0.0.0 --allow-root
+python3 -m jupyter lab \
+    --ContentsManager.allow_hidden=True \
+    --port 5515 --ip=0.0.0.0 --allow-root
