25de099a49e7147d43c0e89d5bfe6f6f15988be7
Kasai Koji <80010105+StreamWest-1629@users.noreply.github.com>
2022-03-14 08:24:50 +0900

    #2: GPU情報を確認するnotebookを追加

---
+++ test/gpu.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # GPU情報の確認
+
+# %% [markdown]
+# パッケージを追加し，CUDAが使えるかどうかを確認する
+
+# %%
+import torch
+
+print(f"cuda is available: {torch.cuda.is_available()}")
+
+# %% [markdown]
+# GPUデバイスの数と現在のデバイスを確認する
+
+# %%
+print(
+    f"cuda devices: {torch.cuda.device_count()} (current device index: {torch.cuda.current_device()})"
+)
+
+# %% [markdown]
+# 現在のGPUについての情報を確認する
+
+# %%
+print(f"current device name: {torch.cuda.get_device_name()}")
+print(f"current device capability: {torch.cuda.get_device_capability()}")
