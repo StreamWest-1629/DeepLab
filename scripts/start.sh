c5fdebf3b542f8f3bf85f63f758840bc60f9ed87
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2022-03-13 23:52:38 +0900

    #2: 完了したtodoの削除

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# TODO: change <bucketname>
-
 goofys -f ${DEEPLAB_BUCKETNAME}:.jupyter/ /root/.jupyter/ &
 goofys -f ${DEEPLAB_BUCKETNAME}:dataset/ /dataset/ &
 python3 -m jupyter lab --port 5515 --ip=0.0.0.0 --allow-root
