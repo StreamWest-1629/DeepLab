4864829b0e278421043a86c5f5d2854ae42e5510
Kasai Koji <streamwest1629@gmail.com>
2022-05-24 18:03:06 +0900

    #78: git-credentialを削除

--- build.dockerfile
+++ build.dockerfile
@@ -5,5 +5,5 @@ RUN git config --global --add safe.directory '*'
 ADD requirements.txt requirements.txt
 RUN --mount=type=cache,target=/root/.cache/pip \
     python3 -m pip install -r requirements.txt
-ADD ./scripts /root/scripts
-RUN chmod +x /root/scripts/start.sh
+ADD ./scripts /entrypoint/scripts
+RUN chmod +x /entrypoint/scripts/start.sh
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,7 @@
 
 python3 -m pip install -r /src/requirements.txt
 
-printenv GIT_CREDENTIALS > /root/.git-credentials
 git config --global credential.helper "store --file /root/.git-credentials"
-git config --global user.name "${GIT_USERNAME}"
-git config --global user.email ${GIT_USERMAIL}
 
 goofys -f --cheap \
     --stat-cache-ttl 30m \
