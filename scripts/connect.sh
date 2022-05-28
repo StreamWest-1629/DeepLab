eae558633de8e953f8867994a23f7ae1aec0eb0c
Kasai Koji <streamwest1629@gmail.com>
2022-05-28 22:57:55 +0900

    #88: スクリプトでログを吐かせる

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,4 +3,4 @@
 INSTANCE_IP=$1
 KEYFILE=$2
 
-ssh -N -R ${INSTANCE_IP}:3443:localhost:3443 ec2-user@${INSTANCE_IP} -i ${KEYFILE}
+ssh -N -v -R ${INSTANCE_IP}:3443:localhost:3443 ec2-user@${INSTANCE_IP} -i ${KEYFILE}
