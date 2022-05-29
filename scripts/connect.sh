db46717acdb1f9c1bec780c253ce34f1cbfd6d3e
Kasai Koji <streamwest1629@gmail.com>
2022-05-30 08:08:23 +0900

    #93: コメントを追加修正

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -2,5 +2,7 @@
 
 INSTANCE_IP=$1
 KEYFILE=$2
-
+cd `dirname $0`/../
 ssh -N -v -R ${INSTANCE_IP}:3443:localhost:3443 ec2-user@${INSTANCE_IP} -i ${KEYFILE}
+
+# Write! `GatewayPorts yes` in /etc/ssh/sshd_config@ec2-instance
