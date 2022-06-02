1b75e90d7197082b319ea16d6457d0c23755473e
Kasai Koji <streamwest1629@gmail.com>
2022-06-02 23:55:46 +0900

    #94: 諸々の修正

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,4 +3,5 @@
 INSTANCE_IP=$1
 KEYFILE=$2
 
-ssh -N -v -R ${INSTANCE_IP}:3443:localhost:3443 ec2-user@${INSTANCE_IP} -i ${KEYFILE}
+# echo "sudo socat tcp4-listen:80,reuseaddr,fork TCP:localhost:3443" | \
+ssh -v -R ${INSTANCE_IP}:3443:localhost:3443 ec2-user@${INSTANCE_IP} -i ${KEYFILE}
